eaf5c86ae81668af85d0435625c004fb418974ab
priyatomar-cmd <priya.tomar@avantika.edu.in>
2026-03-28 18:24:12 +0000

    t5_executed

--- Problem_05.py
+++ Problem_05.py
@@ -1,10 +1,13 @@
-def calculate_fine():
-    book_title=input()
-    days_overdue=int(input())
-    extra_day=int(input())
-    print(days_overdue*extra_day)
-    print(f"Book: {book_title}")
-    print(f"Days overdue: {days_overdue}")
-    fine=float(days_overdue*extra_day)
-    print("Fine: Rs.",fine)
-c=calculate_fine()    
+def calculate_fine(book_title, days_overdue, daily_rate=5.00, max_fine=150.0):
+    fine = days_overdue * daily_rate
+    return fine
+
+
+book_title = input()
+days_overdue = int(input())
+daily_rate = float(input())
+Fine = int(input())
+fine = calculate_fine(book_title, days_overdue,daily_rate)
+print("Book:", book_title)
+print("Days overdue:", days_overdue)
+print("Fine: Rs.", float(fine))
