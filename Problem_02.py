97a81ca7d9b380a08d0bcc22cdf5c5baa8663b6f
priyatomar-cmd <priya.tomar@avantika.edu.in>
2026-03-28 15:14:39 +0000

    Task2_done

---
+++ Problem_02.py
@@ -0,0 +1,10 @@
+def calculate_fine():
+    book_title=input()
+    days_overdue=int(input())
+    extra_day=int(input())
+    print(f"Book:{book_title}")
+    print(f"Days overdue:{days_overdue}")
+    fine=float(days_overdue*extra_day)
+    print("Fine:Rs.",fine)
+c=calculate_fine()    
+    
