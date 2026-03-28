a821d3db3135cbe0535a6bb973500d82c73732e5
priyatomar-cmd <priya.tomar@avantika.edu.in>
2026-03-28 12:43:24 +0000

    t1

--- task_01.py
+++ task_01.py
@@ -4,6 +4,6 @@ def calculate_fine():
     print(f"Book : {book_title}")
     print(f"Days overdue : {days_overdue}")
     fine=float(days_overdue)*5
-    print("Fine : Rs",fine)
+    print("Fine : Rs.",fine)
 c=calculate_fine()    
 
